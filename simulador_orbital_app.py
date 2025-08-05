bac86ca2fd1a343f2522f382f5c90070e3603c21
CiprianoBerretin <cipriano@berretin.com>
2025-08-05 17:22:23 +0200

    Update simulador_orbital_app.py

--- simulador_orbital_app.py
+++ simulador_orbital_app.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
 from matplotlib.collections import LineCollection
-from IPython.display import HTML
+# from IPython.display import HTML
 import matplotlib.cm as cm
 import matplotlib.colors as mcolors
 
